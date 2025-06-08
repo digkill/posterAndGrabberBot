8ceec2b38a91e8402518a55adced1149599bd42d
digkill <digkill@mail.ru>
2025-06-08 19:33:02 +0500

    Change arch

--- cmd/testVKVideo.go
+++ cmd/testVKVideo.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	// Заменить на свою ссылку на видео
+	videoPage := "https://vk.com/video-215383571_456284573"
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", videoPage, nil)
+	// Если видео приватное или требует авторизации — надо подставить свои cookies!
+	// req.Header.Set("Cookie", "remixsid=...; remixlang=0; ...")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	found := false
+	doc.Find("video source").Each(func(i int, s *goquery.Selection) {
+		src, exists := s.Attr("src")
+		if exists && strings.HasSuffix(src, ".mp4") {
+			fmt.Println("MP4 URL:", src)
+			found = true
+		}
+	})
+	if !found {
+		fmt.Println("Не нашёл прямую mp4 ссылку. Возможно, видео приватное или структура страницы изменилась.")
+	}
+}
--- go.mod
+++ go.mod
@@ -3,21 +3,24 @@ module github.com/digkill/posterAndGrabberBot
 go 1.23.3
 
 require (
+	github.com/PuerkitoBio/goquery v1.10.3
 	github.com/cristalhq/aconfig v0.18.6
 	github.com/cristalhq/aconfig/aconfighcl v0.17.1
 	github.com/emirpasic/gods v1.18.1
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/gookit/goutil v0.6.18
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/lib/pq v1.10.9
 	github.com/sashabaranov/go-openai v1.38.0
 )
 
 require (
+	github.com/andybalholm/cascadia v1.3.3 // indirect
 	github.com/gookit/color v1.5.4 // indirect
-	github.com/gookit/goutil v0.6.18 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 )
